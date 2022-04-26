fb79e1df89f13e4a6286a3e837224e1dd91e774b
majid2851 <majidbagheri2851@gmail.com>
2022-04-26 12:26:53 +0430

    navigation-button

--- app/src/main/java/com/example/kotlinnavigationbutton/MainActivity.kt
+++ app/src/main/java/com/example/kotlinnavigationbutton/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.kotlinnavigationbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.kotlinnavigationbutton.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    lateinit var binding:ActivityMainBinding
+    private val homeFragment=HomeFragment()
+    private val profileFragment=ProfileFragment()
+    private val studentFragment=StudentFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        replaceFragment(homeFragment)
+
+        binding.nav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.menuHome -> {
+                    replaceFragment(homeFragment)
+                }
+
+                R.id.menuProfile ->{
+                    replaceFragment(profileFragment)
+                }
+
+                R.id.menuSchool -> {
+                    replaceFragment(studentFragment)
+                }
+            }
+            true
+        }
+
+
     }
+
+    private fun replaceFragment(fragment:Fragment){
+        if (fragment!=null){
+            val transaction=supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainer,fragment).commit()
+
+        }
+
+    }
+
+
+
+
+
+
 }
